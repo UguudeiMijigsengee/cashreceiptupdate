7a93fe98cc8d3bdafa911b4fa181cc1a9ee306e4
UguudeiMijigsengee <44878081+UguudeiMijigsengee@users.noreply.github.com>
2019-09-06 13:56:01 -0500

    Added ignored log files

---
+++ DataAccess/Persistence/Log/ILogRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Model.Log;
+
+namespace DataAccess.Persistence.Log
+{
+    public interface ILogRepository
+    {
+        Task<tblProcessErrorLog> GetLog(int id);
+        void Add(tblProcessErrorLog log);
+        void Remove(tblProcessErrorLog log);
+    }
+}
---
+++ DataAccess/Persistence/Log/LogRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Model.Log;
+
+namespace DataAccess.Persistence.Log
+{
+    public class LogRepository : ILogRepository
+    {
+        private readonly RJWDataDbContext context;
+
+        public LogRepository(RJWDataDbContext context)
+        {
+            this.context = context;
+        }
+        public void Add(tblProcessErrorLog log)
+        {
+            context.tblProcessErrorLogs.Add(log);
+        }
+
+        public async Task<tblProcessErrorLog> GetLog(int id)
+        {
+            return await context.tblProcessErrorLogs.FindAsync(id);
+        }
+
+        public void Remove(tblProcessErrorLog log)
+        {
+            context.Remove(log);
+        }
+    }
+}
